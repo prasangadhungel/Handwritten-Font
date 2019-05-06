d1f19e7387401a37dc71b80663fc98c8aab5c8bd
PrasangaDhungel <dhungel611@gmail.com>
2019-05-06 14:39:23 +0545

    main file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ glutInit()                                             # initialize glut
 glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
 glutInitWindowSize(width, height)                      # set window size
 glutInitWindowPosition(100, 100)                       # set window position
-window = glutCreateWindow("noobtuts.com")              # create window with title
+window = glutCreateWindow("Type  in  your  own  font")              # create window with title
 glutReshapeFunc(reshape)
 glutDisplayFunc(draw)                                  # set draw function callback
 glutIdleFunc(draw)                                     # draw  continuously
