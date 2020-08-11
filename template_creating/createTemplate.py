db106319fa465e0e98b4ca132e06fb3c0e34a96b
a-t-0 <akke.toeter@gmail.com>
2020-08-11 17:20:40 +0200

    Corrected latex template creation.

--- template_creating/createTemplate.py
+++ template_creating/createTemplate.py
@@ -22,6 +22,14 @@ class CreateTemplate:
         self.box_width = 20 #mm
         self.box_spacing = 3 #mm
         
+        # specify qr code line margins
+        self.sheet_top_margin = 6 #measured in mm
+        print(f'self.sheet_top_margin={self.sheet_top_margin}')
+        self.sheet_bottom_margin = self.sheet_top_margin # assumption
+        self.spacing_between_two_lines = self.max_sheet_height*(64/2338) #measured in pixels for box width 20 mm
+        
+        
+        
         # read the symbols from file
         self.symbol_lines = self.read_txt(source_dir,self.symbols_file_name,ext)
         print(f'lines={self.symbol_lines}')
@@ -29,9 +37,13 @@ class CreateTemplate:
         print(f'self.nrOfSymbols={self.nrOfSymbols}')
     
         # generate page distribution params
-        [self.nr_of_boxes_per_line,self.nr_of_lines] = self.optimise_box_distribution()
+        [self.nr_of_boxes_per_line,self.nr_of_lines_in_template] = self.optimise_box_distribution()
         print(f'nr_of_boxes_per_line={self.nr_of_boxes_per_line}')
-        print(f'nr_of_lines={self.nr_of_lines}')
+        print(f'nr_of_lines_in_template={self.nr_of_lines_in_template}')
+    
+        # infer maximum number of lines per page
+        self.max_nr_of_lines_per_page = self.compute_max_nr_of_lines_per_page()
+        print(f'max_nr_of_lines_per_page={self.max_nr_of_lines_per_page}')
     
         # generate page settings
         self.page_setting_lines = self.generate_page_settings()
@@ -93,10 +105,33 @@ class CreateTemplate:
         nr_of_boxes_per_line = math.floor((self.max_sheet_width-self.max_horizontal_margin+self.box_spacing)/ (self.box_width+self.box_spacing))
         print(f'{self.max_sheet_width-self.max_horizontal_margin+self.box_spacing}div{self.box_width+self.box_spacing} = {(self.max_sheet_width-self.max_horizontal_margin+self.box_spacing)/ (self.box_width+self.box_spacing)} [boxes/row]')
         
-        # TODO: implement vertical margin
-        nr_of_lines = round(self.nrOfSymbols/nr_of_boxes_per_line)
-        return [nr_of_boxes_per_line,nr_of_lines]
         
+        
+        #
+        nr_of_lines_in_template = round(self.nrOfSymbols/nr_of_boxes_per_line)
+        return [nr_of_boxes_per_line,nr_of_lines_in_template]
+    
+    # computes the maximum nr of lines that fit on a page
+    def compute_max_nr_of_lines_per_page(self):
+        
+        available_height = self.max_sheet_height-self.sheet_top_margin-self.sheet_bottom_margin
+        remainder_height = available_height
+        height_per_line = self.box_width*3
+        
+        print(f' at start remainder_height={remainder_height}\n\n')
+        # see how many lines you can fit in the available height
+        max_nr_of_lines = 0
+        for i in range(1,self.nrOfSymbols):
+            subtracted = i*height_per_line + (i-1)*self.spacing_between_two_lines
+            print(f'i={i},subtracted={subtracted }')
+            remainder_height = available_height-subtracted 
+            
+            print(f'i={i} and remainder_height={remainder_height}, height_per_line = {height_per_line}, self.spacing_between_two_lines = {self.spacing_between_two_lines}')
+            if remainder_height>0:
+                max_nr_of_lines = i
+            else:
+                break 
+        return max_nr_of_lines
         
     # Put the specificatins in a string array
     def generate_page_settings(self):
@@ -106,28 +141,24 @@ class CreateTemplate:
         lines.append(f'boxHeight = {self.box_width}')
         lines.append(f'nrOfBoxesPerLine = {self.nr_of_boxes_per_line}')
         lines.append(f'nrOfBoxesPerLineMinOne = {self.nr_of_boxes_per_line-1}')
-        lines.append(f'nrOfLinesPerPage = {self.nr_of_lines}')
-        lines.append(f'nrOfLinesPerPageMinOne = {self.nr_of_lines-1}')
+        lines.append(f'nrOfLinesInTemplate = {self.nr_of_lines_in_template}')
+        lines.append(f'nrOfLinesInTemplateMinOne = {self.nr_of_lines_in_template-1}')
+        lines.append(f'maxNrOfLinesPerPage = {self.max_nr_of_lines_per_page}')
         return lines
         # nrOfSymbols = 30
         # boxWidth = 5.2
         # boxHeight = 7.2
         # nrOfBoxesPerLine = 10
         # nrOfBoxesPerLineMinOne = 9
-        # nrOfLinesPerPage = 6
-        # nrOfLinesPerPageMinOne = 5
+        # nrOfLinesInTemplate = 6
+        # nrOfLinesInTemplateMinOne = 5
     
-    
-    def set_box_dimensions(self):
-        self.boxWidth = 5.2
-        self.boxHeight = 7.2
-
 
     def set_box_distribution(self):
         self.nrOfBoxesPerLine = 10
         self.nrOfBoxesPerLineMinOne = 9
-        self.nrOfLinesPerPage = 6
-        self.nrOfLinesPerPageMinOne = 5
+        self.nrOfLinesInTemplate = 6
+        self.nrOfLinesInTemplateMinOne = 5
 
 if __name__ == '__main__':
     main = CreateTemplate()
